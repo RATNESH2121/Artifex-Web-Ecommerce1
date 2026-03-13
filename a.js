e8bafb9da9d6c098fe4de727c96512dddedfb2eb
RATNESH2121 <ratneshdev121218@gmail.com>
2026-03-13 12:40:43 +0530

    Fix BOM issue in Profile.jsx

---
+++ a.js
@@ -0,0 +1,23 @@
+// const sum = (a,b)=>{
+//     console.log(`${a+b}`);
+//     return 1;
+// }
+
+// console.log(sum(2,4));
+
+// const palindrome = (str)=>{
+//     rev="";
+//     for(let i =str.length-1;i>=0;i--){
+//         rev=rev+str[i]+" ";
+//     }
+
+//     return rev;
+// }
+// let str="RATnesh";
+// console.log(palindrome(str));
+
+let a = [1,2,3,4];
+a[1]=213;
+console.log(a);
+let f = new Array(1,2,4);
+console.log(f);
